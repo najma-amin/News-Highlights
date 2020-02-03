b1b1a87053c22c7ab35d837c29c54ea4e602cf44
najmaA <najmaamin10@gmai.com>
2020-02-03 22:23:02 +0300

    models articles added

--- App/models.py
+++ App/models.py
@@ -1,44 +1,25 @@
-class Movie:
+class Sources:
     '''
-    Movie class to define Movie Objects
+    News Source class to define News Source Objects
     '''
 
-    def __init__(self,id,title,overview,poster,vote_average,vote_count):
+    def __init__(self,id,name,description,url,category,country):
         self.id =id
+        self.name = name
+        self.description = description
+        self.url = url
+        self.category = category
+        self.country = country
+        
+class Articles:
+    '''
+    News Articles class to define News Article Objects
+    '''
+    def __init__(self,id,author,title,description,url,urlToImage,publishedAt):
+        self.id = id
+        self.author = author
         self.title = title
-        self.overview = overview
-        self.poster = "https://image.tmdb.org/t/p/w500/" + poster
-        self.vote_average = vote_average
-        self.vote_count = vote_count
-
-
-
-class Review:
-
-    all_reviews = []
-
-    def __init__(self,movie_id,title,imageurl,review):
-        self.movie_id = movie_id
-        self.title = title
-        self.imageurl = imageurl
-        self.review = review
-
-
-    def save_review(self):
-        Review.all_reviews.append(self)
-
-
-    @classmethod
-    def clear_reviews(cls):
-        Review.all_reviews.clear()
-
-    @classmethod
-    def get_reviews(cls,id):
-
-        response = []
-
-        for review in cls.all_reviews:
-            if review.movie_id == id:
-                response.append(review)
-
-        return response
+        self.description = description
+        self.url = url
+        self.urlToImage = urlToImage
+        self.publishedAt = publishedAt
