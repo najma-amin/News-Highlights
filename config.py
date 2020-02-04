ec18b6949b13b0aa68a858ea514bbdede274582b
najmaA <najmaamin10@gmai.com>
2020-02-04 10:18:37 +0300

    configure the blueprint

--- config.py
+++ config.py
@@ -1,34 +1,25 @@
-import os
+import os 
+
 class Config:
-    '''
-    General configuration parent class
-    '''
-    NEWS_API_BASE_URL = 'https://newsapi.org/v2/sources?language=en&category={}&apiKey={}'
-    NEWS_ARTICLES_URL = 'https://newsapi.org/v2/everything?language=en&sources={}&apiKey={}'
-    NEWS_API_KEY = os.environ.get('NEWS_API_KEY')
-    SECRET_KEY = os.environ.get('SECRET_KEY') 
+	NEWS_SOURCES_BASE_URL ='https://newsapi.org/v2/sources?language=en&category={}&apiKey={}'
+	ARTICLES_BASE_URL = 'https://newsapi.org/v2/everything?language=en&sources={}&apiKey={}'
+	NEWS_API_KEY = os.environ.get('fa5e9a55a55645f5a9bc581c1cdd8f99')
+	SECRET_KEY = os.environ.get('najma')
+	
+@staticmethod
+def init_app(app):
+	pass
 
 
 class ProdConfig(Config):
-    '''
-    Production  configuration child class
-    Args:
-        Config: The parent configuration class with General configuration settings
-    '''
     pass
 
 
 class DevConfig(Config):
-    '''
-    Development  configuration child class
-    Args:
-        Config: The parent configuration class with General configuration settings
-    '''
-
     DEBUG = True
 
-
 config_options = {
- 'development':DevConfig,
+'development':DevConfig,
 'production':ProdConfig
+
 }
