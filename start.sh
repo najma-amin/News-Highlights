bfff0f5c738305e70eae5e21cc1d4f0755331349
najmaA <najmaamin10@gmai.com>
2020-02-04 08:27:06 +0300

    added api key for news

---
+++ start.sh
@@ -0,0 +1,4 @@
+ export NEWS_API_KEY='fa5e9a55a55645f5a9bc581c1cdd8f99'
+ export SECRET_KEY='najma'
+
+python3.6 manage.py server
--- tests/news_test.py
+++ tests/news_test.py
@@ -10,13 +10,13 @@ class TestArticles(unittest.TestCase):
         '''
         Test class to run before other tests
         '''
-        self.new_article = Articles(1,'tumaa','Kenyan Man breaks world record','Kipchoge breaks the world record on 2 hour marathon','https://techtoday.com','https://marathons.com/images','2019-010-21T14:50:05Z')
+        self.new_article = Articles(1,'najma','Kenyan Man breaks world record','Kipchoge breaks the world record on 2 hour marathon','https://techtoday.com','https://marathons.com/images','2019-010-21T14:50:05Z')
 
     def test_instance(self):
         self.assertTrue(isinstance(self.new_article,Articles))
 
     def test_to_check_instance_variables(self):
-        self.assertEquals(self.new_article.author,'tumaa')
+        self.assertEquals(self.new_article.author,'najma')
         self.assertEquals(self.new_article.title,'Kenyan Man breaks world record')
         self.assertEquals(self.new_article.description,'Kipchoge breaks the world record on 2 hour marathon')
         self.assertEquals(self.new_article.url,'https://techtoday.com')
